9dbc98e7c08a81e2fe7e96cbdf24e7a914f5979c
zwer9 <ktlusty9@gmail.com>
2024-04-11 22:18:51 +0200

    Pobranie od użytkownika

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Pobierz od użytkownika imie nazwisko i wiek i wypisz Podpowiedzi: nextLine() wczytanie całej lini next() wczytanie słowa
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj dowolna liczbe całkowitą: ");
+        int zmienna = scanner.nextInt();
+        System.out.println("Twoja liczba to: " + zmienna);
+        // kamil ma psa
+        /*
+        ka
+        kskf
+        weronoika
+         */
+    }
+}
